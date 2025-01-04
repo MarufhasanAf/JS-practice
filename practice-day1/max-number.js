39775bc29f7ffd09cddba5b76e08f32ec2f723d9
marufhasanaf <marufhasanaf@gmail.com>
2025-01-05 00:13:02 +0600

    Get max number in an array

---
+++ practice-day1/max-number.js
@@ -0,0 +1,8 @@
+var numbers = [1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10];
+var maxNumber = numbers[0];
+for (var i = 0; i < numbers.length; i++) {
+  if (numbers[i] > maxNumber) {
+    maxNumber = numbers[i];
+  }
+}
+console.log(maxNumber);
